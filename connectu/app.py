55ead67d8f2aafc6b859ff9e4540e0b20d9225e4
Jenna Hlavac <jhlavac@wisc.edu>
2025-11-22 15:26:35 -0600

    Update app.py

--- connectu/app.py
+++ connectu/app.py
@@ -104,4 +104,4 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, host="localhost")
