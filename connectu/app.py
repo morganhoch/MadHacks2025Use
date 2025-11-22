a419b775af1d85d580b788965d3ef7240f6e90df
rebecca-silverstein06 <rebeccasilverstein6@gmail.com>
2025-11-22 14:03:16 -0600

    Update SQLALCHEMY_DATABASE_URI configuration

--- connectu/app.py
+++ connectu/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'connectu.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'connectu.db')
 
 # Database setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/connectu.db'
