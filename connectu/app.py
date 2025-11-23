5658e403a62525eaf814d41c9253e561898a98e8
Jenna Hlavac <jhlavac@wisc.edu>
2025-11-22 20:00:21 -0600

    Update app.py

--- connectu/app.py
+++ connectu/app.py
@@ -7,6 +7,10 @@ import os, secrets
 from models import db, User, DirectMessage, Course  # your models
 from messaging_routes import messaging_bp  # optional if messaging is still used
 from populate_courses import populate_courses
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
 
 # ===== Load environment variables =====
 load_dotenv()
